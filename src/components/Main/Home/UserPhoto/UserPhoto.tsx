626181b9ac5bc8a63a2baf260de95bb895b8aaed
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-06-09 01:07:43 +0400

    user profile picture is add

--- src/components/Main/Home/UserPhoto/UserPhoto.tsx
+++ src/components/Main/Home/UserPhoto/UserPhoto.tsx
@@ -4,8 +4,13 @@ import "./UserPhoto.scss";
 const UserPhoto = () => {
   return (
     <section className="user_photo_container">
-      {/* <img src={UPhoto} alt="User" className="user_photo" /> */}
-      <img src={ShockAbsorber} alt="User" className="user_photo" />
+      <div className="user_photo_cont">
+        <img src={UPhoto} alt="User" className="user_photo" />
+        <div className="user_name_id">
+            <p className="user_name">Cyber Samurai #177</p>
+        </div>
+      </div>
+      {/* <img src={ShockAbsorber} alt="User" className="user_photo" /> */}
     </section>
   );
 };
